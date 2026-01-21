82f22b5a314080a7535f167993353d32e8a0570a
Ivane-Ba <ivanbagashvili@gmail.com>
2026-01-21 14:19:34 +0100

    Refactor XOR decoder script for improved readability and functionality

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-decoding=$(echo $1 | sed -e "s/{xor}//g" | base64 --decode)
-b64_clean=$(echo "$decoding" | tr -d \\n | od -An -tx1 | tr -dc '[[:alnum:]]')
-size=${#b64_clean}
-for (( i=0; i<size; i+=2 )); do
-	hexa_xor_underscore=$((16#${b64_clean:i:2} ^ 95))
-	result+=$(printf '\\x%x' $hexa_xor_underscore)
+# Decode XOR WebSphere encoded password
+# Usage: ./1-xor_decoder.sh {xor}ENCODED_VALUE
+
+result=""
+
+decoding=$(echo "$1" | sed 's/{xor}//g' | base64 --decode)
+hex_stream=$(echo "$decoding" | tr -d '\n' | od -An -tx1 | tr -dc '[[:alnum:]]')
+size=${#hex_stream}
+
+for (( i=0; i<size; i+=2 ))
+do
+	byte=$((16#${hex_stream:i:2} ^ 95))
+	result+=$(printf '\\x%x' "$byte")
 done
-echo -e $result
+
+echo -e "$result"
